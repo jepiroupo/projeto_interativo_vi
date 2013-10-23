170239ffe77cf845e8f3136826188dd6507ceb9b
jepiroupo <jessicapiroupo@hotmail.com>
2013-10-23 12:19:08 -0200

    Pasta Allegro (Wander, por e-mail)

--- allegro/main.c
+++ allegro/main.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <allegro5/allegro.h>
 
-const float FPS = 15;
+const float FPS = 80;
 const int SCREEN_W = 1415;
 const int SCREEN_H = 734;
-const int SELETOR_SIZE = 57;
+const int SELETOR_SIZE = 97;
 const int margem_x = 15;
 const int margem_y = 15;
 enum MYKEYS {
    KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT
 };
+const int VERMELHO = 2;
+const int AZUL = 1;
+const int VERDE = 3;
+const int AMARELO = 4;
 
 ALLEGRO_BITMAP *parede;
 ALLEGRO_BITMAP *caminho;
@@ -23,6 +27,12 @@ ALLEGRO_BITMAP *pa_amarelo;
 ALLEGRO_BITMAP *pa_vermelho;
 ALLEGRO_BITMAP *background;
 
+ALLEGRO_BITMAP *cachorro;
+/* 0 - Cima | 1 - Direita | 2 - Baixo | 3 - Esquerda */
+int direcao_atual = 3;
+int pos_x_cachorro = 0;
+int pos_y_cachorro = 0;
+
 void lerConf(int mapa[770][3])
 {
 	char linha;
@@ -31,12 +41,12 @@ void lerConf(int mapa[770][3])
 
 	int pos_x, pos_y, tipo;
 	char aux_p[4];
-	char aux_t[2];
+	char aux_t[3];
 	int contador = 0;
 	
 	int contador_item = 0;
 
-	fp = fopen("map/map2.txt","r");
+	fp = fopen("map/map1.txt","r");
 
 	if( fp == NULL )
 	{
@@ -50,17 +60,11 @@ void lerConf(int mapa[770][3])
 		if(contador_item == 770){break;}
 
 		if(contador < 4)
-		{
 			aux_p[contador] = linha;
-		}
 		else if(contador < 8)
-		{
 			aux_p[contador - 4] = linha;
-		}
 		else
-		{
-			aux_t[0] = linha;
-		}
+			aux_t[contador - 8] = linha;
 
 		contador++;
 
@@ -74,7 +78,7 @@ void lerConf(int mapa[770][3])
 			sscanf(aux_p, "%d", &pos_y);
 			mapa[contador_item][1] = pos_y;
 		}
-		else if(contador == 9)
+		else if(contador == 10)
 		{
 			contador = 0;
 			sscanf(aux_t, "%d", &tipo);
@@ -100,9 +104,69 @@ void inicializaBitmaps()
 	pa_verde = al_load_bitmap("img/pa_verde.png");
 	pa_amarelo = al_load_bitmap("img/pa_amarelo.png");
 	pa_vermelho = al_load_bitmap("img/pa_vermelho.png");
+
+	cachorro = al_load_bitmap("img/cachorro/cachorro_frente_0001.png");
 }
 
-int desenhaMapa(int mapa[770][3])
+bool validaProximo(int mapa[770][3], int posicao)
+{
+	if(mapa[posicao][2] == 0)
+		return true;
+	else if(mapa[posicao][2] == 6)
+		return true;
+	else if(mapa[posicao][2] == 7)
+		return true;
+	else if(mapa[posicao][2] == 8)
+		return true;
+	else if(mapa[posicao][2] == 9)
+		return true;
+	else if(mapa[posicao][2] == 10)
+		return true;
+	return false;
+}
+
+void mudaDirecao()
+{
+	direcao_atual = rand() % 4;
+}
+
+int desenhaCachorro(int mapa[770][3])
+{
+	int aux_x = pos_x_cachorro/32;
+	int aux_y = pos_y_cachorro/32;
+	int prox_pos = 0;
+
+//	printf("%d - y>%d | x>%d \n", direcao_atual, aux_y, aux_x);
+
+	if(direcao_atual == 0)
+	{
+		prox_pos = ((aux_y-1)*35)+aux_x;
+		if(validaProximo(mapa, prox_pos))
+			pos_y_cachorro = pos_y_cachorro - 32;
+	}
+	else if(direcao_atual == 1)
+	{
+		prox_pos = (aux_y*35)+aux_x+1;
+		if(validaProximo(mapa, prox_pos))
+			pos_x_cachorro = pos_x_cachorro + 32;
+	}
+	else if(direcao_atual == 2)
+	{
+		prox_pos = ((aux_y+1)*35)+aux_x;
+		if(validaProximo(mapa, prox_pos))
+			pos_y_cachorro = pos_y_cachorro + 32;
+	}
+	else if(direcao_atual == 3)
+	{
+		prox_pos = (aux_y*35)+aux_x-1;
+		if(validaProximo(mapa, prox_pos))
+			pos_x_cachorro = pos_x_cachorro - 32;
+	}
+	al_draw_bitmap(cachorro, pos_x_cachorro + margem_x, pos_y_cachorro + margem_y, 0);
+	mudaDirecao();
+}
+
+int desenhaMapa(int mapa[770][3], bool envio[4])
 {
 	int x, y, tipo;
 
@@ -117,15 +181,118 @@ int desenhaMapa(int mapa[770][3])
 		else if(tipo == 1)
 			al_draw_bitmap(parede, x + margem_x, y + margem_y, 0);
 		else if(tipo == 2)
-			al_draw_bitmap(p_vermelho, x + margem_x, y + margem_y, 0);
+		{
+			if(envio[VERMELHO] == false)
+			{
+				al_draw_bitmap(p_vermelho, x + margem_x, y + margem_y, 0);
+			}
+			else
+			{
+				mapa[i][2] = 6;
+				al_draw_bitmap(pa_vermelho, x + margem_x, y + margem_y, 0);
+			}
+		}
 		else if(tipo == 3)
-			al_draw_bitmap(p_azul, x + margem_x, y + margem_y, 0);
+		{
+			if(envio[AZUL] == false)
+			{
+				al_draw_bitmap(p_azul, x + margem_x, y + margem_y, 0);
+			}
+			else
+			{
+				mapa[i][2] = 7;
+				al_draw_bitmap(pa_azul, x + margem_x, y + margem_y, 0);
+			}
+		}
 		else if(tipo == 4)
-			al_draw_bitmap(p_verde, x + margem_x, y + margem_y, 0);
+		{
+			if(envio[VERDE] == false)
+			{
+				al_draw_bitmap(p_verde, x + margem_x, y + margem_y, 0);
+			}
+			else
+			{
+				mapa[i][2] = 8;
+				al_draw_bitmap(pa_verde, x + margem_x, y + margem_y, 0);
+			}
+		}
 		else if(tipo == 5)
-			al_draw_bitmap(p_amarelo, x + margem_x, y + margem_y, 0);
+		{
+			if(envio[AMARELO] == false)
+			{
+				al_draw_bitmap(p_amarelo, x + margem_x, y + margem_y, 0);
+			}
+			else
+			{
+				mapa[i][2] = 9;
+				al_draw_bitmap(pa_amarelo, x + margem_x, y + margem_y, 0);
+			}
+		}
+		else if(tipo == 6)
+		{
+			if(envio[VERMELHO] == false)
+			{
+				al_draw_bitmap(pa_vermelho, x + margem_x, y + margem_y, 0);
+			}
+			else
+			{
+				mapa[i][2] = 2;
+				al_draw_bitmap(p_vermelho, x + margem_x, y + margem_y, 0);
+			}
+		}
+		else if(tipo == 7)
+		{
+			if(envio[AZUL] == false)
+			{
+				al_draw_bitmap(pa_azul, x + margem_x, y + margem_y, 0);
+			}
+			else
+			{
+				mapa[i][2] = 3;
+				al_draw_bitmap(p_azul, x + margem_x, y + margem_y, 0);
+			}
+		}
+		else if(tipo == 8)
+		{
+			if(envio[VERDE] == false)
+			{
+				al_draw_bitmap(pa_verde, x + margem_x, y + margem_y, 0);
+			}
+			else
+			{
+				mapa[i][2] = 4;
+				al_draw_bitmap(p_verde, x + margem_x, y + margem_y, 0);
+			}
+		}
+		else if(tipo == 9)
+		{
+			if(envio[AMARELO] == false)
+			{
+				al_draw_bitmap(pa_amarelo, x + margem_x, y + margem_y, 0);
+			}
+			else
+			{
+				mapa[i][2] = 5;
+				al_draw_bitmap(p_amarelo, x + margem_x, y + margem_y, 0);
+			}
+		}
+		else if(tipo == 10 && pos_x_cachorro == 0 && pos_y_cachorro == 0)
+		{
+			printf("i: %d - x: %d - y: %d\n", i, x, y);
+			pos_x_cachorro = x;
+			pos_y_cachorro = y;
+			al_draw_bitmap(cachorro, x + margem_x, y + margem_y, 0);
+		}
 	}
 
+	if(envio[VERMELHO] == true)
+		envio[VERMELHO] = false;
+	if(envio[AZUL] == true)
+		envio[AZUL] = false;
+	if(envio[VERDE] == true)
+		envio[VERDE] = false;
+	if(envio[AMARELO] == true)
+		envio[AMARELO] = false;
 }
 
 int main(int argc, char **argv)
@@ -136,16 +303,16 @@ int main(int argc, char **argv)
     ALLEGRO_BITMAP *seletor = NULL;
 
     float tamanho = 25;
-    float espacamento = SELETOR_SIZE + 4;
-    float seletor_x = 678;
-    float seletor_y = 5;
+    float espacamento = SELETOR_SIZE + 19;
+    float seletor_x = 1174;
+    float seletor_y = 40;
     bool key[4] = { false, false, false, false };
     bool redraw = true;
     bool doexit = false;
     float posicao = 0;
 	int mapa[770][3];
+    bool envio[4] = { false, false, false, false };
 
-	
 
 	//Inicia o allegro
     al_init();
@@ -174,10 +341,11 @@ int main(int argc, char **argv)
 
 	//Carrega a imagem do seletor na variável
     seletor = al_load_bitmap("img/selecao.png");
-	//Desenha o seletor na posição (658,5)
-    al_draw_bitmap(seletor, 658, 5, 0);
+	//Desenha o seletor na posição (1174,40)
+    al_draw_bitmap(seletor, 1174, 40, 0);
 
-	desenhaMapa(mapa);
+	desenhaMapa(mapa, envio);
+	desenhaCachorro(mapa);
 
     //Registra os eventos
     al_register_event_source(event_queue, al_get_display_event_source(display));
@@ -276,6 +444,16 @@ int main(int argc, char **argv)
                 case ALLEGRO_KEY_DOWN:
                     key[KEY_DOWN] = false;
                     break;
+				case ALLEGRO_KEY_ENTER:
+					if(posicao == 0)
+						envio[VERMELHO] = true;
+					else if(posicao == 1)
+						envio[AZUL] = true;
+					else if(posicao == 2)
+						envio[VERDE] = true;
+					else if(posicao == 3)
+						envio[AMARELO] = true;
+					
             }
         }
 
@@ -284,8 +462,9 @@ int main(int argc, char **argv)
         {
             redraw = false;
             al_draw_bitmap(background, 0, 0, 0);
+			desenhaMapa(mapa, envio);
+			desenhaCachorro(mapa);
             al_draw_bitmap(seletor, seletor_x, seletor_y, 0);
-			desenhaMapa(mapa);
             al_flip_display();
         }
     }
@@ -299,5 +478,3 @@ int main(int argc, char **argv)
 
     return 0;
 }
-
-// gcc -o teste main.c -lallegro_image -lallegro $(pkg-config --cflags --libs allegro-5.0 allegro_primitives-5.0)
